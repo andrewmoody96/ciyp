4101f01f799f34d9284f582dca443e2984e4ed62
Andrew <andrew.moody96@gmail.com>
2023-11-28 20:05:18 -0600

    fixed dayjs issue

--- client/src/components/Feature/index.js
+++ client/src/components/Feature/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import dayjs from "dayjs";
-import FirstSingle from "./Project1/FirstSingle";
-import SecondSingle from "./Project1/SecondSingle";
 import ThirdSingle from "./Project1/ThirdSingle";
 import Project1 from "./Project1/Project1";
 
@@ -11,13 +9,9 @@ export default function Feature() {
 
     if (now >= process.env.REACT_APP_PROJECT1_DATE) {
       return <Project1 />;
-    } else if (now >= process.env.REACT_APP_SINGLE3_DATE) {
+    } else {
       return <ThirdSingle />;
-    } else if (now >= process.env.REACT_APP_SINGLE2_DATE) {
-      return <SecondSingle />;
-    } else if (now >= process.env.REACT_APP_SINGLE1_DATE) {
-      return <FirstSingle />;
-    }
+    } 
   };
 
   return <>{renderRelease()}</>;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,39 +1,17 @@
-import { React, useEffect, useState } from "react";
-// import Default from "./default";
+import React from "react";
+import Default from "./default";
 import Feature from "../Feature/index";
 import dayjs from "dayjs";
 
-let now = dayjs().format("MM-DD-YYYY").toString();
-
-console.log(now);
-
 export default function Home() {
-  // const [view, setView] = useState();
-
-  // useEffect(() => {
-  //   let importantDate = dayjs(`${process.env.REACT_APP_SINGLE1_DATE}`)
-  //     .format("MM-DD-YYYY")
-  //     .toString();
-
-  //   const featureCheck = () => {
-  //     let now = dayjs().format("MM-DD-YYYY").toString();
-
-  //     if (now >= importantDate) {
-  //       setView("featured");
-  //     } else {
-  //       setView("");
-  //     }
-  //   };
-  //   featureCheck();
-  // }, []);
-
   const homeView = () => {
-    return (<Feature />)
-    // if (view === "featured") {
-    //   return <Feature />;
-    // } else {
-    //   return <Default />;
-    // }
+    let now = dayjs().format("MM-DD-YYYY").toString();
+
+    if (now >= process.env.REACT_APP_SINGLE1_DATE) {
+      return <Feature />;
+    } else {
+      return <Default />;
+    }
   };
 
   return (
