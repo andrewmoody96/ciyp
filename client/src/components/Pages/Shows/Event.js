3861d6ef03bf08331bf7cad547ced09b2c735724
Andrew <andrew.moody96@gmail.com>
2024-10-25 19:01:52 -0500

    nav improvement, tvStatic, event links**

--- client/src/components/Pages/Shows/Event.js
+++ client/src/components/Pages/Shows/Event.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 export default function Event({ description }) {
-  // console.log(description);
   let date = description[0];
   let venue = description[1];
   let time = description[2];
   let location = description[3];
+  let url = null;
+  if (description.length < 4) {
+    url = description[4];
+  }
+  console.log(description);
+
   return (
     <div className="flex flex-col w-[80vw] tablet:w-[50vw] desktop:w-[30vw]">
       <div className="m-2 px-2 rounded-2xl shadow-[inset_0_1px_5px_0_rgb(0,0,0);] items-center bg-[#B36551]/90 border-4 border-black">
@@ -20,6 +25,13 @@ export default function Event({ description }) {
             <h2 className="m-1 text-center text-sm text-white font-moda">
               {location}
             </h2>
+            {url !== null && (
+              <h2 className="m-1 text-center text-md text-white font-moda">
+                <a target="_blank" rel="noreferrer" href={url}>
+                  TICKETS
+                </a>
+              </h2>
+            )}
           </div>
         </span>
       </div>
--- client/src/components/Reusable/Menu/nav.js
+++ client/src/components/Reusable/Menu/nav.js
@@ -11,28 +11,28 @@ export default function Nav() {
     <>
       <div className="nav">
         <ul
-          style={{ display: burgerOpen ? "inline" : "none" }}
-          className="bg-[#262626] border-r border-b rounded-br-md h-auto w-[50vw] mt-[3.1rem] absolute font-moda text-white"
+          style={{ display: burgerOpen ? "flex" : "none" }}
+          className="flex-col items-center tablet:justify-center bg-[#262626] border-r border-b rounded-br-md h-[100vh] w-[50vw] tablet:h-[50vh] tablet:w-[25vw] mt-[3.1rem] absolute font-moda text-white text-4xl "
         >
-          <button className="font-moda m-2 p-[5px] rounded-lg hover:text-[#B36551]">
-            <Link to="/">Home</Link>
+          <button className="m-2 p-[5px] rounded-lg hover:text-[#B36551]">
+            <Link reloadDocument to="/">Home</Link>
           </button>
-          {/* Shows, Email Signup */}
-          <button className="font-moda m-2 p-[5px] rounded-lg hover:text-[#B36551]">
-            <a href="#shows">Shows</a>
+          {/*Reloads Home Page*/}
+          <button className="m-2 p-[5px] rounded-lg hover:text-[#B36551]">
+            <a onClick={toggleBurger} href="#shows">Shows</a>
           </button>
           {/* Scroll to Shows Section */}
-          <button className="font-moda m-2 p-[5px] rounded-lg hover:text-[#B36551]">
-            <Link to="/contact">Booking</Link>
+          <button className="m-2 p-[5px] rounded-lg hover:text-[#B36551]">
+            <a onClick={toggleBurger} href="#subscribe">Subscribe</a>
           </button>
-          {/* Email Booking, Instagram Link */}
-          <button className="font-moda m-2 p-[5px] rounded-lg hover:text-[#B36551]">
+          {/* Subscribe to Email, Social Link */}
+          <button className="m-2 p-[5px] rounded-lg hover:text-[#B36551]">
             <Link target="_blank" rel="noreferrer" to="https://linktr.ee/chocolateinyourpocket">Listen</Link>
           </button>
           {/* Link to LinkTree */}
         </ul>
         <div
-          className="hamburger flex items-center  justify-center tablet:hidden"
+          className="hamburger flex items-center  justify-center desktop:hidden"
           onClick={toggleBurger}
         >
           <Hamburger isOpen={burgerOpen} />
