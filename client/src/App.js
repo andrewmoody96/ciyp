3bde743ef6382525f4e9a1777a8225337269f961
Andrew <andrew.moody96@gmail.com>
2024-12-02 13:57:20 -0600

    removed deps

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,6 @@ import Header from "./components/Reusable/Header";
 import Footer from "./components/Reusable/Footer";
 import Home from "./components/Pages/Home";
 import NotFound from "./components/Pages/NotFound";
-import Music from "./components/Pages/Music";
-import BackToLife from "./components/Pages/Music/backtolife/More";
-import Melting from "./components/Pages/Music/melting/More";
-import DieTrying from "./components/Pages/Music/dietrying/More";
 
 export default function App() {
   return (
@@ -17,10 +13,6 @@ export default function App() {
         <div className="h-[75vh] w-[85vw] flex justify-center">
           <Routes className="overflow-y-auto">
             <Route path="/" element={<Home />} />
-            <Route path="/music" element={<Music />} />
-            <Route path="/backtolife" element={<BackToLife />} />
-            <Route path="/melting" element={<Melting />} />
-            <Route path="/dietrying" element={<DieTrying />} />
             <Route path="/*" element={<NotFound />} />
           </Routes>
         </div>
