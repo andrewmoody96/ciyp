7136c28a64dd65fa6d41afc0b56f082b2afccd55
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2026-04-14 02:11:54 -0500

    removing unncessary dependencies (#101)
    * migrating to TypeScript
    
    * new build using .env variables
    
    * removed unnessary packages

--- app/root.tsx
+++ app/root.tsx
@@ -22,13 +22,6 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  {
-    rel: "stylesheet",
-    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-  },
-  { rel: "stylesheet", href: "./assets/fontawesome/css/fontawesome.css" },
-  { rel: "stylesheet", href: "./assets/fontawesome/css/brands.css" },
-  { rel: "stylesheet", href: "./assets/fontawesome/css/solid.css" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
